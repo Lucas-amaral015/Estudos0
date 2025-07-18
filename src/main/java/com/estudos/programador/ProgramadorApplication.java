2cf8aaa5d559ca9c2f5461df10721721ec013127
Lucas Amaral <Lucasam@outlook.com>
2025-07-18 11:37:48 -0300

    a partir daqui se inicia o aprendizado

---
+++ src/main/java/com/estudos/programador/ProgramadorApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.programador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class ProgramadorApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("eaii");
+
+	}
+}
---
+++ src/test/java/com/estudos/programador/ProgramadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.programador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
