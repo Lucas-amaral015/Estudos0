31fe9336ed0ec1b1ec18c4f945e3680c2fcb6b49
Lucas Amaral <Lucasam@outlook.com>
2025-07-24 09:36:03 -0300

    Estudando e aplicando interação de usuários
    #exercício 1

--- src/main/java/com/estudos/programador/ProgramadorApplication.java
+++ src/main/java/com/estudos/programador/ProgramadorApplication.java
@@ -3,11 +3,32 @@ package com.estudos.programador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 public class ProgramadorApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("eaii");
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite a nota 1: ");
+		double nota1 = sc.nextDouble();
+
+		System.out.println("Digite a nota 2: ");
+		double nota2 = sc.nextDouble();
+
+		System.out.println("Digite a nota 3: ");
+		double nota3 = sc.nextDouble();
+
+		double media = (nota1 + nota2 + nota3) /3;
+
+		System.out.println("média final: " + media);
+
+
+
+
+
 
 	}
+
 }
---
+++ src/main/java/com/estudos/programador/explicacao/Explicacao.java
@@ -0,0 +1,6 @@
+package com.estudos.programador.explicacao;
+
+public class Explicacao {
+    //Scanner é para introduzir a inteiração do usuário.
+    //instanciamos(criar) objetos a partir dele.
+}
