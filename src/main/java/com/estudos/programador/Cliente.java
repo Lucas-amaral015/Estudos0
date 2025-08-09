aaf3730f17cf4c3fffdafd53cbd8a63a03097ac4
Lucas Amaral <Lucasam@outlook.com>
2025-08-08 22:47:39 -0300

    Aplicando POO com o seguinte pilar: Encapsulamento.

--- src/main/java/com/estudos/programador/Cliente.java
+++ src/main/java/com/estudos/programador/Cliente.java
@@ -3,18 +3,61 @@ package com.estudos.programador;
 
 public class Cliente {
 
-    /*aqui se aplica herança, onde a classe mãe é Cliente e as filhas
-    são: EmpresasCnpj e PessoaFisica.
-
+    /*aqui se aplica o encapsulamento para proteger as variáveis.
+    - uso de getters e setters;
+    - get - pegar/mostrar informações
+    - set - alterar ou incluir informações;
+    - encapsular ele cria barreiras para não ser acessado em qualquer lugar
+    - cria abstração.
      */
 
-    String name;
-    String cpf;
-    double income;
-    int age;
-    String cnpj;
+    private String name;
+    private String cpf;
+    private double income;
+    private int age;
+    private String cnpj;
 
     public void mensagemGenerica(){
         System.out.println("Cadastro realizado com sucesso!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
     }
 }
--- src/main/java/com/estudos/programador/ProgramadorApplication.java
+++ src/main/java/com/estudos/programador/ProgramadorApplication.java
@@ -11,17 +11,17 @@ public class ProgramadorApplication {
 	public static void main(String[] args) {
 
 		PessoaFisica pessoa = new PessoaFisica();
-		pessoa.cpf = "48177720856";
-		pessoa.age = 25;
-		pessoa.name = "Lucas Amaral";
-		pessoa.income = 3000.00;
+		pessoa.setCpf("48177720856");
+		pessoa.setAge(25);
+		pessoa.setName("Lucas Amaral");
+		pessoa.setIncome(3000.0);
 		pessoa.mensagemPf();
 		pessoa.mensagemGenerica();
 		pessoa.mensagemGenerica(2);
 
 		EmpresaCnpj empresa = new EmpresaCnpj();
-		empresa.cnpj = "35211420859";
-		empresa.income = 100000.00;
+		empresa.setCnpj("35211420859");
+		empresa.setIncome(100000.00);
 		empresa.mensagemPj();
 		empresa.mensagemGenerica();
 	}
